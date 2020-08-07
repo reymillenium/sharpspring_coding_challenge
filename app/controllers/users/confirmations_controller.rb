a47504a5a894a693fa494c3751a8e49ccdede81c
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-07 03:59:32 -0400

    Copied the devise views and constrollers using the users scope. Mod the devise initializer by setting: 'config.scoped_views = true'

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,12 +1,45 @@
+# frozen_string_literal: true
+
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
 
-  private
+  # POST /resource/confirmation
+  # def create
+  #   super
+  # end
 
+  # GET /resource/confirmation?confirmation_token=abcdef
+  # def show
+  #   super
+  # end
+
+  # protected
+
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
-    # super
-    sign_in(resource)
-    notes_path
-  end
+    super(resource_name, resource)
 
-  # layout 'login'
+    # sign_in(resource)
+    # notes_path
+  end
 end
+
+
+# class Users::ConfirmationsController < Devise::ConfirmationsController
+#
+#   private
+#
+#   def after_confirmation_path_for(resource_name, resource)
+#     sign_in(resource)
+#     notes_path
+#   end
+#
+# end
