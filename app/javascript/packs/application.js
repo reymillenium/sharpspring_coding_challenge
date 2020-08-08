70718ed855dc6edfeb6691a6019ee40e247cb77e
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-07 22:52:22 -0400

    Added bootstrap layout. Modified table. Added functionality to nav-bar and side-bar links.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ require("channels")
 
 import "@fortawesome/fontawesome-free/js/all";
 
+// require("/app/javascript/scripts")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
