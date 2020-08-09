5a683ac98efcc76fb51cd540292e72900f462cee
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-09 01:07:26 -0400

    Added extra fields to the users table

---
+++ db/migrate/20200809050351_add_more_fields_to_users_table.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddMoreFieldsToUsersTable < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string, default: ""
+    add_column :users, :last_name, :string, default: ""
+  end
+end
