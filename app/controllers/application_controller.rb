2a97ee242b8a03a448eadf5a2b58423a09f2f571
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-08 22:34:27 -0400

    Fixed root routes and redirectioning after singin and signout on the application_controller (with the methods 'after_sign_in_path_for' & 'after_sign_out_path_for'). Added the login layout. Modified the sessions new view by using the new login layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::Base
 
+  layout :layout_by_resource
+
   before_action :authenticate_user!
 
+  def layout_by_resource
+    if devise_controller?
+      "login"
+      # elsif controller_name == 'terms_acceptances' && action_name != 'index'
+      #   "basic"
+      # elsif controller_name == "lab_scans" || cookies["app"] == "lab_scan"
+      #   "lab_scans"
+    else
+      "application"
+    end
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    notes_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_session_path(resource_or_scope)
+  end
 
 end
